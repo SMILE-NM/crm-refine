6c09b0b53bddd68b5aa0557e2e40b9d36efa3cc6
SMILE-NM <123355875+SMILE-NM@users.noreply.github.com>
2024-08-01 20:08:16 +0500

    companies page

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import UpcomingEvents from './home/upcoming-events';
+import DealsChart from './home/deals-chart';
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import ProjectCardSkeleton from './skeleton/project-card';
+import DashboardTotalCountCard from './home/total-count-card';
+import LatestActivities from './home/latest-activities';
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  DashboardTotalCountCard,
+  LatestActivities,
+};
