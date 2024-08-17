6e943bed42b68c5fb36ded1612926d8a9fad6bda
SMILE-NM <nematovmz2003@gmail.com>
2024-08-17 10:30:58 +0500

    Create future completed

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -15,7 +15,12 @@ import {
 import { getDefaultFilter, useGo } from '@refinedev/core';
 import { Input, Space, Table } from 'antd';
 
-export const CompanyList = () => {
+interface CompanyTestInterface {
+  name: string;
+  avatarUrl: string;
+  dealsAggregate: any;
+}
+export const CompanyList = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
   const { tableProps, filters } = useTable({
     resource: 'companies',
@@ -48,68 +53,72 @@ export const CompanyList = () => {
   });
 
   return (
-    <List
-      breadcrumb={false}
-      headerButtons={() => (
-        <CreateButton
-          onClick={() => {
-            go({
-              to: {
-                resource: 'companies',
-                action: 'create',
-              },
-              options: {
-                keepQuery: true,
-              },
-              type: 'replace',
-            });
-          }}
-        />
-      )}
-    >
-      <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
-        <Table.Column<Company>
-          dataIndex="name"
-          title="Company Title"
-          defaultFilteredValue={getDefaultFilter('id', filters)}
-          filterIcon={<SearchOutlined />}
-          filterDropdown={(props) => (
-            <FilterDropdown {...props}>
-              <Input placeholder="Search Company" />
-            </FilterDropdown>
-          )}
-          render={(value, record) => (
-            <Space>
-              <CustomAvatar
-                shape="square"
-                name={record.name}
-                src={record.avatarUrl}
-              />
-              <Text style={{ whiteSpace: 'nowrap' }}>{record.name}</Text>
-            </Space>
-          )}
-        />
-        <Table.Column<Company>
-          dataIndex="totalRevenue"
-          title="Open deals amount"
-          render={(value, company) => (
-            <Text>
-              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
-            </Text>
-          )}
-        />
-        <Table.Column<Company>
-          dataIndex="totalRevenue"
-          title="Actions"
-          fixed="right"
-          render={(value, company) => (
-            <Space>
-              <EditButton hideText size="small" recordItemId={value} />
-              <DeleteButton hideText size="small" recordItemId={value} />
-            </Space>
-          )}
-        />
-      </Table>
-    </List>
+    <div>
+      <List
+        breadcrumb={false}
+        headerButtons={() => (
+          <CreateButton
+            onClick={() => {
+              console.log('Create');
+              go({
+                to: {
+                  resource: 'companies',
+                  action: 'create',
+                },
+                options: {
+                  keepQuery: true,
+                },
+                type: 'replace',
+              });
+            }}
+          />
+        )}
+      >
+        <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
+          <Table.Column<CompanyTestInterface>
+            dataIndex="name"
+            title="Company Title"
+            defaultFilteredValue={getDefaultFilter('id', filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(value, record) => (
+              <Space>
+                <CustomAvatar
+                  shape="square"
+                  name={record.name}
+                  src={record.avatarUrl}
+                />
+                <Text style={{ whiteSpace: 'nowrap' }}>{record.name}</Text>
+              </Space>
+            )}
+          />
+          <Table.Column<CompanyTestInterface>
+            dataIndex="totalRevenue"
+            title="Open deals amount"
+            render={(value, company) => (
+              <Text>
+                {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+              </Text>
+            )}
+          />
+          <Table.Column<CompanyTestInterface>
+            dataIndex="id"
+            title="Actions"
+            fixed="right"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+      {children}
+    </div>
   );
 };
